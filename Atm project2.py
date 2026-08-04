2c3c020824a9fe4141a1df7add9c7f91b5103c60
sainigourav9627-creator <sainigourav9627@gmail.com>
2026-08-04 14:25:13 +0530

    Update Atm project2.py

--- Atm project2.py
+++ Atm project2.py
@@ -1,33 +1,53 @@
-Current_Balance=1000
-print("============= ATM Project ====================")
+ATM_PIN = 1234
+pin = int(input("Enter ATM PIN: "))
 
-print("\n1.Check Balance")
-print("2.Deposit")
-print("3.Withdraw")
-print("4.Exit")
+if pin == ATM_PIN:
 
-choice = int(input("\nEnter Your Choice: "))
+    Current_Balance = 1000
 
+    while True:
 
-if choice == 1:
-    print("Current Balance =", Current_Balance)
+        print("===== ATM =====")
+        print("1. Check Balance")
+        print("2. Deposit")
+        print("3. Withdraw")
+        print("4. Exit")
 
-elif choice == 2:
-    amount = int(input("Enter Deposit Amount: "))
-    Current_Balance = Current_Balance + amount
-    print("Current Balance =", Current_Balance)
+        choice = int(input("Enter Choice: "))
 
-elif choice == 3:
-    amount = int(input("Enter Withdraw Amount: "))
+        if choice == 1:
+            print("Current Balance =", Current_Balance)
 
-    if amount <= Current_Balance:
-        Current_Balance = Current_Balance - amount
-        print("Current Balance =", Current_Balance)
-    else:
-        print("Insufficient Balance")
+        elif choice == 2:
+            amount = int(input("Enter Deposit Amount: "))
 
-elif choice == 4:
-    print("Exit")
+            if amount > 0:
+                Current_Balance = Current_Balance + amount
+                print("Current Balance =", Current_Balance)
+            else:
+                print("Invalid Amount")
+
+        elif choice == 3:
+            amount = int(input("Enter Withdraw Amount: "))
+
+            if amount > 0:
+
+                if amount <= Current_Balance:
+                    Current_Balance = Current_Balance - amount
+                    print("Current Balance =", Current_Balance)
+
+                else:
+                    print("Insufficient Balance")
+
+            else:
+                print("Invalid Amount")
+
+        elif choice == 4:
+            print("Thank You")
+            break
+
+        else:
+            print("Invalid Choice")
 
 else:
-    print("Invalid Choice")
+    print("Wrong PIN")
